88be6e8a4fb91ce4f0b07a0f797830c96cbf2c31
kevin <kevinyay945@gmail.com>
2021-10-03 20:29:02 +0800

    feat: 新增command crud api

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module smart-home-backend
 go 1.16
 
 require (
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/echo/v4 v4.5.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,92 @@
 package main
 
 import (
+	"fmt"
+	"github.com/google/uuid"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo"
 )
 
+type HttpSuccessResponse struct {
+	Status string `json:"status"`
+	Data interface{} `json:"data"`
+}
+
+type Command struct {
+	Uuid string `json:"uuid"`
+	CreateAt time.Time `json:"createAt"`
+	UpdateAt time.Time `json:"updateAt"`
+	Url string `json:"url"`
+}
+var allCommand []Command = []Command{{
+				"bbbad37f-c6cd-47f7-907d-add1c4045559",
+				time.Now(),
+				time.Now(),
+				"http://example.com",
+			},{
+			"bbbad37f-c6cd-47f7-907d-add1c4045558",
+			time.Now(),
+			time.Now(),
+			"http://example2.com",
+			}}
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.GET("/commands", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, HttpSuccessResponse{
+			Status: "success",
+			Data: allCommand,
+		})
+	})
+
+	e.POST("/commands", func(c echo.Context) error {
+		input := new(Command)
+		if err := c.Bind(input); err != nil {
+			return c.String(http.StatusBadRequest, "Fail to Bind Data")
+		}
+		_uuid, uuidErr := uuid.NewRandom()
+		if uuidErr != nil {
+			return c.String(http.StatusInternalServerError, "Fail to generate uuid")
+		}
+		input.Uuid = _uuid.String()
+		input.CreateAt = time.Now()
+		input.UpdateAt = time.Now()
+	return c.JSON(http.StatusOK, HttpSuccessResponse{
+			Status: "success",
+			Data: input,
+		})
+	})
+
+	e.PUT("/commands/:uuid", func(c echo.Context) error {
+		input := new(Command)
+		if err := c.Bind(input); err != nil {
+			return c.String(http.StatusBadRequest, "Fail to Bind Data")
+		}
+		commandUuid := c.Param("uuid")
+		output := new(Command)
+		for _, command := range allCommand {
+			if commandUuid == command.Uuid {
+				output = &command
+			}
+		}
+		return c.JSON(http.StatusOK, HttpSuccessResponse{
+			Status: "success",
+			Data: output,
+		})
+	})
+
+	e.DELETE("/commands/:uuid", func(c echo.Context) error {
+		commandUuid := c.Param("uuid")
+		fmt.Println("Delete uuid", commandUuid)
+		return c.JSON(http.StatusOK, HttpSuccessResponse{
+			Status: "success",
+			Data: nil,
+		})
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
