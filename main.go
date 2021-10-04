ed48749fe6d79949674734c08bb039a219389da6
kevin <kevinyay945@gmail.com>
2021-10-04 20:41:33 +0800

    feat: 新增request restful api

--- main.go
+++ main.go
@@ -91,6 +91,90 @@ func main() {
 	})
 
 	e.DELETE("/commands/:uuid", func(c echo.Context) error {
+		requestUuid := c.Param("uuid")
+		fmt.Println("Delete uuid", requestUuid)
+		return c.JSON(http.StatusOK, HttpSuccessResponse{
+			Status: "success",
+			Data: nil,
+		})
+	})
+
+	type Request struct {
+		Uuid string `json:"uuid"`
+		CreateAt time.Time `json:"createAt"`
+		UpdateAt time.Time `json:"updateAt"`
+		Name string `json:"name"`
+	}
+
+	var allRequest []Request = []Request{{
+		"324e672e-3512-41b0-97dc-065c334f8f7a",
+		time.Now(),
+		time.Now(),
+		"Request 1",
+	},{
+		"6737a85c-3ea4-48ed-bfed-4bdff74b189b",
+		time.Now(),
+		time.Now(),
+		"Request 2",
+	}}
+
+	e.GET("/requests", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, HttpSuccessResponse{
+			Status: "success",
+			Data: struct {
+				Request []Request `json:"requests"`
+			}{
+				Request: allRequest,
+			},
+		})
+	})
+
+	e.POST("/requests", func(c echo.Context) error {
+		input := new(Request)
+		if err := c.Bind(input); err != nil {
+			return c.String(http.StatusBadRequest, "Fail to Bind Data")
+		}
+		_uuid, uuidErr := uuid.NewRandom()
+		if uuidErr != nil {
+			return c.String(http.StatusInternalServerError, "Fail to generate uuid")
+		}
+		input.Uuid = _uuid.String()
+		input.CreateAt = time.Now()
+		input.UpdateAt = time.Now()
+		return c.JSON(http.StatusOK, HttpSuccessResponse{
+			Status: "success",
+			Data: struct {
+				Request Request `json:"request"`
+			}{Request: *input},
+		})
+	})
+
+	e.PUT("/requests/:uuid", func(c echo.Context) error {
+		input := new(Request)
+		if err := c.Bind(input); err != nil {
+			return c.String(http.StatusBadRequest, "Fail to Bind Data")
+		}
+		requestUuid := c.Param("uuid")
+		output := new(Request)
+		for _, request := range allRequest {
+			if requestUuid == request.Uuid {
+				output.Uuid = request.Uuid
+				output.CreateAt = request.CreateAt
+				output.Name = input.Name
+				output.UpdateAt = time.Now()
+			}
+		}
+		return c.JSON(http.StatusOK, HttpSuccessResponse{
+			Status: "success",
+			Data: struct {
+				Request Request `json:"request"`
+			}{
+				Request: *output,
+			},
+		})
+	})
+
+	e.DELETE("/requests/:uuid", func(c echo.Context) error {
 		commandUuid := c.Param("uuid")
 		fmt.Println("Delete uuid", commandUuid)
 		return c.JSON(http.StatusOK, HttpSuccessResponse{
